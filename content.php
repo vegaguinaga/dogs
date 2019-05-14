3787dca00769ef6da5e99e5dab3ee5d41904431a
Jose Vega <vegaguinaga@gmail.com>
2019-05-14 15:56:55 -0600

    Creacion de temas en WP I

---
+++ content.php
@@ -0,0 +1,36 @@
+  <article class="Content">
+    <h1>Creación de Temas</h1>
+    <?php if(have_posts()): while(have_posts()):the_post();?>
+      <article>
+      <img src="<?php echo get_the_post_thumbnail_url();?>" alt="<?php echo get_the_title(); ?>">
+      <h2>
+      <a href="<?php the_permalink();?>"><?php the_title();?></a>
+      </h2>
+      <?php the_excerpt(); ?>
+      <p><?php the_category(' , '); ?></p>
+      <p><?php the_tags(); ?></p>
+      <p>
+      <?php the_time(); ?>
+      <br>
+      <?php the_time('d-M-Y'); ?>
+      <br>
+      <?php the_time(get_option('date_format'));?>
+      </p>
+      <p>
+      <?php the_author(); ?>
+      <?php the_author_posts_link(); ?>
+      </p>
+      <div class="the-content">
+        <?php the_content(); ?>
+      </div>
+      </article>
+      <hr>
+    <?php endwhile; else:?>
+      <p>El contenido solicitado no existe</p>
+    <?php endif;?>
+  </article>
+  <section class="Pagination">
+    <?php //previous_post_link();?>
+    <?php //next_post_link();?>
+    <?php echo paginate_links(); ?>
+  </section>
---
+++ scripts.js
@@ -0,0 +1,5 @@
+;
+console.log('Hello WordPress');
+
+// Para evitar conflictos con versiones de JQuery entre WordPress y sus plugins
+$ = jQuery.noConflict();
