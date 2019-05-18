1472f71f4ae15659bf500efac1b5176761256afd
Jose Vega <vegaguinaga@gmail.com>
2019-05-17 20:37:48 -0600

    Condictional tags

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php 
 get_header();
+if(is_home()){
+  echo '<mark>Estoy en el home</mark>';
+} elseif (is_category()) {
+  echo '<mark>Estoy mostrando resultados de categorías</mark>';
+} elseif (is_tag()) {
+  echo '<mark>Estoy mostrando resultados de etiquetas</mark>';
+}
 get_template_part('content');
 get_sidebar();
 get_footer();
